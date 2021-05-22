1527738d710d40aff6cf6cb8654ce61ab18e4497
gobalakrishnan1996 <gobalakrishnangv@gmail.com>
2021-05-22 18:55:56 +0530

    Subtract

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+num1 = float
+num2 = float
+total = float
+
+def main():
+    print ("This program subtracts one number from another" + ".")
+    num1 = input ("Enter first number: ")
+    num1 = float (num1)
+    num2 = input ("Enter second number: ")
+    num2 = float (num2)
+    total = num1 - num2
+    print ("The result is" + " " + str(total))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
